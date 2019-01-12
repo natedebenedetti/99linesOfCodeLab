d5864b47d7962e82138f1961abff365695e58ec0
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-01-12 16:10:54 -0500

    everything done except the last line in the 99 lines.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var friends = ["matt", "brooks", "dustin", "jordan", "chip"];
 
+
 getFriend(friends);
 
 
-function getFriend() {
+function getFriend() { //function that lists each name in the friends array and for each name sings the song in decending order starting at 99.
     for (f = 0; f < friends.length; f++) {
         console.log(friends[f]);
-         for (l = 99; l <= 100 && l >= 1; --l){
-            console.log(l); 
+         for (l = 99; l <= 100 && l >= 2; --l){
+            var li = l;
+            console.log(l + " lines of code in the file, " + l + " lines of code; John strikes one out, clears it all out, " + li + " lines of code in the file"); 
         }
         
     }
 }
-console.log("program ended");
+
 
 
 
