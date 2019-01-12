6ca64846ba124753b8743a2bdcc964ce8ab84054
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-01-12 18:47:00 -0500

    finished

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ getFriend(friends);
 function getFriend() { //function that lists each name in the friends array and for each name sings the song in decending order starting at 99.
     for (f = 0; f < friends.length; f++) {
         console.log(friends[f]);
-         for (l = 99; l <= 99 && l >= 2; --l){
-            var li = l;
+        for (l = 99; l <= 99 && l >= 1; --l) {
+            var li = l -1;
             var name = friends[f];
-            console.log(l + " lines of code in the file, " + l + " lines of code; " +  name + " strikes one out, clears it all out, " + li + " lines of code in the file"); 
-        } 
-        
+            if (li >= 1) {
+                console.log(l + " lines of code in the file, " + l + " lines of code; " + name + " strikes one out, clears it all out, " + li + " lines of code in the file");
+            } else {
+               console.log(l + " line of code in the file, 1 line of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
+            }
+        }
+
     }
 }
 
