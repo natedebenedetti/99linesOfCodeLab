126ffef935f7f59fdcab14824ef20ea66cfc7271
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-01-12 15:15:06 -0500

    lists friends name and counts down from 99 to 1 for each friend

---
+++ app.js
@@ -0,0 +1,18 @@
+var friends = ["matt", "brooks", "dustin", "jordan", "chip"];
+
+getFriend(friends);
+
+
+function getFriend() {
+    for (f = 0; f < friends.length; f++) {
+        console.log(friends[f]);
+         for (l = 99; l <= 100 && l >= 1; --l){
+            console.log(l); 
+        }
+        
+    }
+}
+console.log("program ended");
+
+
+
